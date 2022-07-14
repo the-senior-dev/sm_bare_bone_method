9400ab13fef0daaf238505ad961e0746ed0a95a6
Bogdan Nedelcu <bogdanned32@gmail.com>
2022-07-14 11:40:58 +0200

    fix lint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import styled from "styled-components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./views/MainPage";
