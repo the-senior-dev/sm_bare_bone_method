e127eb3e72caf10f9657293c3a3d1efa32de6ce8
Bogdan Nedelcu <bogdanned32@gmail.com>
2022-07-15 17:16:14 +0200

    feat: connect Context to state, add the provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./views/MainPage";
 import MoviePage from "./views/MoviePage";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import { DarkModeContext, Theme, themeList, ThemeName } from "./store/context";
+import styled from "styled-components";
 
 export default function App() {
+  const [activeTheme, setActiveTheme] = useState(themeList.light);
+
   return (
-    <AppContainer>
-      <Header></Header>
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/movie/:id" element={<MoviePage />}></Route>
-        </Routes>
-      </Router>
-      <Footer></Footer>
-    </AppContainer>
+    <DarkModeContext.Provider
+      value={{
+        theme: activeTheme,
+        setTheme: (themeKey: ThemeName) => {
+          setActiveTheme(themeList[themeKey] as Theme);
+        },
+      }}
+    >
+      <AppContainer>
+        <Header></Header>
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/movie/:id" element={<MoviePage />}></Route>
+          </Routes>
+        </Router>
+        <Footer></Footer>
+      </AppContainer>
+    </DarkModeContext.Provider>
   );
 }
 
